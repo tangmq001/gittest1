d1e8707ab55467ae0100f83da10e58da42a78d4d
tang <2427077090@qq.com>
2019-04-07 19:32:30 +0800

    test

---
+++ src/main/java/com/tang/Main.java
@@ -0,0 +1,12 @@
+package com.tang;
+
+/**
+ * Created by 唐明巧 on 2019/4/7.
+ *
+ * @Note：
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("i have a dream!");
+    }
+}
